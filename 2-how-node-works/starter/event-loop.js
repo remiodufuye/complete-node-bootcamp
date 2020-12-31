58f0cd74c1d13fd10a5598a9c5709e6f5064e892
Remi <Remi.Odufuye@gmail.com>
2020-12-31 14:31:00 -0500

    changes made

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -1,15 +1,20 @@
 
-const fs = require('fs')
+const fs = require('fs');
+
 
 setTimeout(() => console.log('Timer 1 Finished'),0) ; 
 setImmediate(()=> console.log('Immediate 1 Finished!'));
 
 fs.readFile('test-file.txt', () => {
     console.log('I/O Finished') ; 
+    console.log('#################')
 
     setTimeout(() => console.log('Timer 2 Finished'),0) ; 
     setTimeout(() => console.log('Timer 3 Finished'),3000)
     setImmediate(()=> console.log('Immediate 2 Finished!'));
+
+    process.nextTick(()=>console.log('Process.nextTick')) ; 
+    
 
 })
 
